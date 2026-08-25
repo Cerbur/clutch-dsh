0b22c6bc470e1d4ef5e5877c29f79890f5d431b0
cerbur <cerburyc@gmail.com>
2026-08-25 11:06:09 +0800

    chore(worktree): release package

--- packages/clutch-dsh-worktree/test/package-manifest.test.mjs
+++ packages/clutch-dsh-worktree/test/package-manifest.test.mjs
@@ -1,6 +1,7 @@
 import assert from 'node:assert/strict';
 import { readFile } from 'node:fs/promises';
 import test from 'node:test';
+import { URL } from 'node:url';
 
 const packageManifest = JSON.parse(
   await readFile(new URL('../package.json', import.meta.url), 'utf8'),
